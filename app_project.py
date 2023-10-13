8fc4bc83116b295fb357061ee5d593aa14ad68e9
Ambar C Rodriguez <60482817+AmbarCRoAl@users.noreply.github.com>
2023-10-13 10:01:28 -0400

    Update app_project.py

--- app_project.py
+++ app_project.py
@@ -13,21 +13,21 @@ with st.expander('More information'):
 if 'file' not in st.session_state:
   st.session_state('file')='not done'
 def file_uploaded():
-   st.session_state('file')='done'
+  st.session_state('file')='done'
 file_uploaded = col2.file_uploader('Upload the census file here', on_change=file_uploaded)
 if  st.session_state('file') == 'done':
-    col2.sucess('File uploaded sucessfully')
-           
-    df_census = file_uploaded
-    gain_age = sns.jointplot(y='capital-gain', x='age', data=df_census, kind="reg", color="#7f1a1a")
-    loss_age = sns.jointplot(y='capital-loss', x='age', data=df_census, kind="reg", color="#7f1a1a")
-    fnlwgt_age = sns.jointplot(y='fnlwgt', x='age', data=df_census, kind="reg", color="#7f1a1a")
-    hours_age = sns.jointplot(y='hours-per-week', x='age', data=df_census, kind="reg", color="#7f1a1a")
-    hours_gain = sns.jointplot(y='hours-per-week', x='capital-gain', data=df_census, kind="reg", color="#7f1a1a")
+  col2.sucess('File uploaded sucessfully')
+         
+  df_census = file_uploaded
+  gain_age = sns.jointplot(y='capital-gain', x='age', data=df_census, kind="reg", color="#7f1a1a")
+  loss_age = sns.jointplot(y='capital-loss', x='age', data=df_census, kind="reg", color="#7f1a1a")
+  fnlwgt_age = sns.jointplot(y='fnlwgt', x='age', data=df_census, kind="reg", color="#7f1a1a")
+  hours_age = sns.jointplot(y='hours-per-week', x='age', data=df_census, kind="reg", color="#7f1a1a")
+  hours_gain = sns.jointplot(y='hours-per-week', x='capital-gain', data=df_census, kind="reg", color="#7f1a1a")
 
-    col2.markdown('Some exploratory graphs for the raw data')
-    col2.image(gain_age)
-    col2.image(loss_age)
-    col2.image(fnlwgt_age)
-    col2.image(hours_age)
-    col2.image(hours_gain)
+  col2.markdown('Some exploratory graphs for the raw data')
+  col2.image(gain_age)
+  col2.image(loss_age)
+  col2.image(fnlwgt_age)
+  col2.image(hours_age)
+  col2.image(hours_gain)
