d12a795201610a9d60e7adc119844afd25d64c5a
Ambar C Rodriguez <60482817+AmbarCRoAl@users.noreply.github.com>
2023-10-17 12:18:02 -0400

    Update 2_Exploration.py

--- pages/2_Exploration.py
+++ pages/2_Exploration.py
@@ -1,1 +1,17 @@
 import streamlit as st
+
+
+
+
+
+
+
+
+
+st.write('# Age and gender')
+
+st.write('# Working sector and education')
+
+st.write('# Country of origin')
+
+st.write('# Marital Status')
