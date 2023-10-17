a0fe2e913df90bd0fa79bf531e21d80cec70328c
Ambar C Rodriguez <60482817+AmbarCRoAl@users.noreply.github.com>
2023-10-17 11:06:53 -0400

    Update app_project.py

--- app_project.py
+++ app_project.py
@@ -2,10 +2,10 @@ import streamlit as st
 import seaborn as sns
 
 Home.py # This is the file you run with "streamlit run"
-└─── pages/
-  └─── Introduction.py # This is a page
-  └─── 2_Exploration.py # This is another page
-  └─── 3_Conclusion.py # So is this
+  pages/
+    Introduction.py # This is a page
+    2_Exploration.py # This is another page
+    3_Conclusion.py # So is this
 
 col1, col2 = st.columns([1,2]) #Creates 2 columns where the one in the middle is double the size that the other
 col1.markdown('# App for census information') #The hash makes it a title
