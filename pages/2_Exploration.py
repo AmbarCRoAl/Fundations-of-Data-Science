e77b675c5c7cddf4e2fa93ea68385399b1c89a9f
Ambar C Rodriguez <60482817+AmbarCRoAl@users.noreply.github.com>
2023-10-17 22:16:01 -0400

    Update 2_Exploration.py

--- pages/2_Exploration.py
+++ pages/2_Exploration.py
@@ -19,7 +19,7 @@ if sd == "Age and gender":
         st.write('# Age and gender')
         col1, col2 = st.columns(2)
         col1.image('https://raw.githubusercontent.com/AmbarCRoAl/Fundations-of-Data-Science/main/age_sex1.png')
-        st.markdown('Write something to separate the graphs.')
+        col1, col2 = st.columns(2)
         col1.image('https://raw.githubusercontent.com/AmbarCRoAl/Fundations-of-Data-Science/main/age_sex2.png')
         col2.image('https://raw.githubusercontent.com/AmbarCRoAl/Fundations-of-Data-Science/main/age_sex3.png')
 
