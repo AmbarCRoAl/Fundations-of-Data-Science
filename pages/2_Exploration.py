b233b9ef9e521623854f1ec8011a91e252b96490
Ambar C Rodriguez <60482817+AmbarCRoAl@users.noreply.github.com>
2023-10-17 21:59:31 -0400

    Update 2_Exploration.py

--- pages/2_Exploration.py
+++ pages/2_Exploration.py
@@ -16,32 +16,28 @@ sd = st.selectbox("Please select one:", #Drop Down Menu Name
     )
 
 if sd == "Age and gender":
-    st.write('# Age and gender')
-    fig = graph_age_percentage()
-    st.pyplot(fig)
-    col1, col2 = st.columns(2)
-    fig = graph_age_gains()
-    col1.pyplot(fig)
-    fig = graph_age_losses()
-    col2.pyplot(fig)
+        st.write('# Age and gender')
+        st.image('https://raw.githubusercontent.com/AmbarCRoAl/Fundations-of-Data-Science/main/age_sex1.png')
+        col1, col2 = st.columns(2)
+        col1.image('https://raw.githubusercontent.com/AmbarCRoAl/Fundations-of-Data-Science/main/age_sex2.png')
+        col2.image('https://raw.githubusercontent.com/AmbarCRoAl/Fundations-of-Data-Science/main/age_sex3.png')
+
 
 elif sd == "Working sector and education":
-    st.write('# Working sector and education')
-    fig = graph_jobedu_gains()
-    st.pyplot(fig)
-    fig = graph_jobedu_losses()
-    st.pyplot(fig)
+        st.write('# Working sector and education')
+        col2.image('https://raw.githubusercontent.com/AmbarCRoAl/Fundations-of-Data-Science/main/educ_sect1.png')
+        col2.image('https://raw.githubusercontent.com/AmbarCRoAl/Fundations-of-Data-Science/main/educ_sect2.png')
+
 
 elif sd == "Country of origin":
-    st.write('# Country of origin')
-    col1, col2 = st.columns(2)
-    fig = graph_countr_percentage()
-    col1.pyplot(fig)
-    fig = graph_countr_gainloss()
-    col2.pyplot(fig)
+        st.write('# Country of origin')
+        col1, col2 = st.columns(2)
+        col2.image('https://raw.githubusercontent.com/AmbarCRoAl/Fundations-of-Data-Science/main/country1.png')
+        col2.image('https://raw.githubusercontent.com/AmbarCRoAl/Fundations-of-Data-Science/main/country2.png')
+
 
 elif sd == "Marital status":
-    st.write('# Marital status"')
-    fig = graph_marital_gainloss()
-    st.pyplot(fig)
+        st.write('# Marital status"')
+        col2.image('https://raw.githubusercontent.com/AmbarCRoAl/Fundations-of-Data-Science/main/marital1.png')
+
 
