87d25afaf80ef167b6885487cd452000fa2b24fc
Ambar C Rodriguez <60482817+AmbarCRoAl@users.noreply.github.com>
2023-10-31 22:15:38 -0400

    Update Introduction.py

--- Introduction.py
+++ Introduction.py
@@ -614,5 +614,5 @@ def color_coding(row):
         row) if row['Median income'] <= person_info[0] else ['background-color:green'] * len(row)
 table = {'Group':median_keys, 'Median income':median_info }
 df_table = pd.DataFrame(table)
-st.dataframe(df.style.apply(color_coding, axis=1))
+st.dataframe(df_table.style.apply(color_coding, axis=1))
 st.dataframe(df_table)
