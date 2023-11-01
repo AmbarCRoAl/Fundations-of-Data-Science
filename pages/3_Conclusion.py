dfc8a349455edd8302e8052a430dc724742593ba
Ambar C Rodriguez <60482817+AmbarCRoAl@users.noreply.github.com>
2023-10-31 21:43:49 -0400

    Update 3_Conclusion.py

--- pages/3_Conclusion.py
+++ pages/3_Conclusion.py
@@ -256,6 +256,8 @@ for i in range(num):
 url = 'https://raw.githubusercontent.com/AmbarCRoAl/Fundations-of-Data-Science/main/Post-tax%20household%20income%20summary_transverse.csv'
 df_summary = pd.read_csv(url)
 
+name = "Summary table with all the percentages and distributed capital gains and capital losses for the different groups"
+st.markdown(f"""  #### <span style="color:green">{name}</span>  """,  unsafe_allow_html=True)
 table = {'Group':groups, 'Percentage of high earners':ratio_people,
          'Normalized gain for high earners':ratio_gain_over,
          'Normalized loss for high earners':ratio_loss_over,
