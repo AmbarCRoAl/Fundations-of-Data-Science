073c83af413eea055be0d2883f5919483f49b167
Ambar C Rodriguez <60482817+AmbarCRoAl@users.noreply.github.com>
2023-10-31 23:17:37 -0400

    Update 3_Conclusion.py

--- pages/3_Conclusion.py
+++ pages/3_Conclusion.py
@@ -300,5 +300,5 @@ table = {'Group':groups, 'Percentage of high earners':ratio_people,
          'Normalized gain for low earners':ratio_gain_under,
          'Normalized loss for low earners':ratio_loss_under   }
 df_table = pd.DataFrame(table)
-st.dataframe(df_table.style.highlight_max(axis=0))
+st.dataframe(df_table.style.highlight_max(colors=green))
 
