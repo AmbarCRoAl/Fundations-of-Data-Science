47f65e1618ece2d31d13aa40e3ff07a0503a6daa
Ambar C Rodriguez <60482817+AmbarCRoAl@users.noreply.github.com>
2023-10-31 21:53:01 -0400

    Update Introduction.py

--- Introduction.py
+++ Introduction.py
@@ -75,12 +75,13 @@ st.markdown(''':gray[&emsp;Please enter yes or no.] ''')
 person_income = st.text_input('Anual income:')
 st.markdown(''':gray[&emsp;Please enter your post-tax anual income without any symbol.] ''')
 
+st.markdown(''':red[Please complete every box to see the rest.] ''')
+st.markdown(''':red[Note that if you don't use the specific words provided, the information won't be displayed.] ''')
+
 
 person_age = int(person_age)
 person_income = float(person_income)
 person_info = [person_income, person_age, person_education, person_country, person_sex, person_marital, person_industry]
-
-st.markdown(''':red[Note that if you don't use the specific words provided, the information won't be displayed.] ''')
 
 
 
