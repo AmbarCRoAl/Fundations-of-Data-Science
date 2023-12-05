5316341e62621f013edc67625036a26c32b6cd2c
Ambar C Rodriguez <60482817+AmbarCRoAl@users.noreply.github.com>
2023-12-05 16:31:40 -0500

    Update 3_Prediction.py

--- midterm/pages/3_Prediction.py
+++ midterm/pages/3_Prediction.py
@@ -2,6 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import streamlit as st
 
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
