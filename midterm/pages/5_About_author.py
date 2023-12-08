8b5ac2bfbf9da87926b07f3c352730d60c367ed4
Ambar C Rodriguez <60482817+AmbarCRoAl@users.noreply.github.com>
2023-12-08 12:41:50 -0500

    Update 5_About_author.py

--- midterm/pages/5_About_author.py
+++ midterm/pages/5_About_author.py
@@ -4,8 +4,9 @@ col1, col2 = st.columns([1,2])
 col1.image("https://raw.githubusercontent.com/AmbarCRoAl/Fundations-of-Data-Science/main/midterm/IMG_2649.jpg", width=200)
 col2.markdown("<h1 style='text-align: left; color: darkolivegreen;'> Ambar C. Rodriguez Alicea </h1>", unsafe_allow_html=True)
  
-st.markdown( """<h2 style='text-align: center;font-size: 14px;'> I'm a mother of a 4yo girl; we were both born and raised in Puerto Rico. We moved to Michigan last year so I could start a graduate degree in Experimental 
+st.markdown( """<h2 style='text-align: center;font-size: 16px;'> I'm a mother of a 4yo girl; we were both born and raised in Puerto Rico. We moved to Michigan last year so I could start a graduate degree in Experimental 
 Nuclear Physics. 
+
 
 Throughout my studies, I've done research related to neural networks, accelerator measurements, design and commissioning of detection systems, 
 first principle calculations and material science. My Masters thesis aims to propose a system to perform electron elastic scattering off rare 
