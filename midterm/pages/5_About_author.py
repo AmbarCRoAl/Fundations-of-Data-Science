96ce385f05dd5f168bf798517d424e468ae92326
Ambar C Rodriguez <60482817+AmbarCRoAl@users.noreply.github.com>
2023-12-08 10:37:46 -0500

    Update 5_About_author.py

--- midterm/pages/5_About_author.py
+++ midterm/pages/5_About_author.py
@@ -1,9 +1,8 @@
 import streamlit as st
 
 col1, col2 = st.columns([1,2]) #Creates 2 columns where the one in the middle is double the size that the other
-col1.markdown("<h1 style='text-align: left; color: darkolivegreen;'> Correlation of people's attributes and their income</h1>", unsafe_allow_html=True) 
-
-col2.image("https://raw.githubusercontent.com/AmbarCRoAl/Fundations-of-Data-Science/main/midterm/IMG_2649.jpg")
+col1.markdown("<h1 style='text-align: left; color: darkolivegreen;'> Ambar C. Rodriguez Alicea </h1>", unsafe_allow_html=True) 
+col2.image("https://raw.githubusercontent.com/AmbarCRoAl/Fundations-of-Data-Science/main/midterm/IMG_2649.jpg", width=400)
 
 
  
