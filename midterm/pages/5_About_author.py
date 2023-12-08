1a4d5ce633f187e4e3f7cda893f583d52d494210
Ambar C Rodriguez <60482817+AmbarCRoAl@users.noreply.github.com>
2023-12-08 12:40:33 -0500

    Update 5_About_author.py

--- midterm/pages/5_About_author.py
+++ midterm/pages/5_About_author.py
@@ -5,9 +5,13 @@ col1.image("https://raw.githubusercontent.com/AmbarCRoAl/Fundations-of-Data-Scie
 col2.markdown("<h1 style='text-align: left; color: darkolivegreen;'> Ambar C. Rodriguez Alicea </h1>", unsafe_allow_html=True)
  
 st.markdown( """<h2 style='text-align: center;font-size: 14px;'> I'm a mother of a 4yo girl; we were both born and raised in Puerto Rico. We moved to Michigan last year so I could start a graduate degree in Experimental 
-Nuclear Physics. Throughout my studies, I've done research related to neural networks, accelerator measurements, design and commissioning of detection systems, 
+Nuclear Physics. 
+
+Throughout my studies, I've done research related to neural networks, accelerator measurements, design and commissioning of detection systems, 
 first principle calculations and material science. My Masters thesis aims to propose a system to perform electron elastic scattering off rare 
-isotopes at my university's accelerator complex; the Facility for Rare Isotope Beams. I like learning, and spending time with family and friends. 
+isotopes at my university's accelerator complex; the Facility for Rare Isotope Beams. 
+
+I like learning, and spending time with family and friends. 
 The things that excite me most about the future are working in the development and operation of nuclear power plants, and watching my daughter grow. 
 
 :dizzy: 
