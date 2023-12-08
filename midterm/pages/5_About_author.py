b4585cdd9740b19e34c03cb945437d736de8a50d
Ambar C Rodriguez <60482817+AmbarCRoAl@users.noreply.github.com>
2023-12-08 10:40:04 -0500

    Update 5_About_author.py

--- midterm/pages/5_About_author.py
+++ midterm/pages/5_About_author.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-col1, col2 = st.columns(2) 
-col2.markdown("<h1 style='text-align: left; color: darkolivegreen;'> Ambar C. Rodriguez Alicea </h1>", unsafe_allow_html=True) 
+col1, col2 = st.columns(2)  
 col1.image("https://raw.githubusercontent.com/AmbarCRoAl/Fundations-of-Data-Science/main/midterm/IMG_2649.jpg", width=200)
+col2.markdown("<h1 style='text-align: left; color: darkolivegreen;'> Ambar C. Rodriguez Alicea </h1>", unsafe_allow_html=True)
 
 
  
