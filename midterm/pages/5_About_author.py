23b1b0742bb356a655a008b03e67c1c8c606e627
Ambar C Rodriguez <60482817+AmbarCRoAl@users.noreply.github.com>
2023-12-08 10:28:00 -0500

    Add files via upload

---
+++ midterm/pages/5_About_author.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+col1, col2 = st.columns([1,2]) #Creates 2 columns where the one in the middle is double the size that the other
+col1.markdown("<h1 style='text-align: left; color: darkolivegreen;'> Correlation of people's attributes and their income</h1>", unsafe_allow_html=True) 
+
+col2.image("https://raw.githubusercontent.com/AmbarCRoAl/Fundations-of-Data-Science/main/midterm/IMG_2649.jpg")
+
+
+ 
